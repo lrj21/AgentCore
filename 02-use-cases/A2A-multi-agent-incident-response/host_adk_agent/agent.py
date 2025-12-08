2605da3f37d785ba663b9b456c9c1018a12425d3
ren.jie.li <ren.jie.li@renjielndiannao.cn.ibm.com>
2025-12-08 10:51:44 +0800

    update model id

--- 02-use-cases/A2A-multi-agent-incident-response/host_adk_agent/agent.py
+++ 02-use-cases/A2A-multi-agent-incident-response/host_adk_agent/agent.py
@@ -10,7 +10,7 @@ import os
 import uuid
 
 IS_DOCKER = os.getenv("DOCKER_CONTAINER", "0") == "1"
-GOOGLE_MODEL_ID = os.getenv("GOOGLE_MODEL_ID", "arn:aws:bedrock:us-west-2:345568587821:inference-profile/us.amazon.nova-pro-v1:0")
+GOOGLE_MODEL_ID = os.getenv("GOOGLE_MODEL_ID", "us.amazon.nova-pro-v1:0")
 
 if IS_DOCKER:
     from utils import get_ssm_parameter, get_aws_info
